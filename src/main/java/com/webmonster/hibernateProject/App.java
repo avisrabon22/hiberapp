a5023a1708f151e7516b8543687012d444efea4b
avisrabon22@gmail.com <avisrabon22@gmail.com>
2021-12-29 19:36:56 +0530

    apdate on sql table update

--- src/main/java/com/webmonster/hibernateProject/App.java
+++ src/main/java/com/webmonster/hibernateProject/App.java
@@ -1,19 +1,30 @@
 package com.webmonster.hibernateProject;
-
-
+//import####################################################
 import org.hibernate.cfg.Configuration;
 
+//main @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Project started...");
+		Configuration cfg = new Configuration();
+		cfg.configure();
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Project started..." );
-        Configuration cfg= new Configuration();
-        cfg.configure();
-        
-     org.hibernate.SessionFactory factory=cfg.buildSessionFactory();
-     System.out.println(factory);
-     System.out.println(factory.isClosed());
-    }
+		org.hibernate.SessionFactory factory = cfg.buildSessionFactory();
+//     System.out.println(factory);
+//     System.out.println(factory.isClosed());
+
+		Users user = new Users();
+		
+		user.setId(1);
+		user.setFullname("Avijit Maity");
+		user.setEmail("avisrabon22@gmail.com");
+		user.setPassword("Avijit@3834");
+//######################################################################
+		org.hibernate.Session session = factory.openSession();
+		org.hibernate.Transaction tx = session.beginTransaction();
+		session.save(user);
+		tx.commit();
+		session.close();
+
+	}
 }
--- src/main/java/com/webmonster/hibernateProject/Users.java
+++ src/main/java/com/webmonster/hibernateProject/Users.java
@@ -1,14 +1,59 @@
 package com.webmonster.hibernateProject;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Users {
 
+	@Id
 	private int id;
 	private String fullname;
 	private String email;
 	private String password;
 	
-	
-//	@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//***************************************************************************
+public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getFullname() {
+		return fullname;
+	}
+
+
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	//	@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 	public Users(int id, String fullname, String email, String password) {
 		super();
 		this.id = id;
@@ -21,6 +66,12 @@ public class Users {
 	public Users() {
 		super();
 		// TODO Auto-generated constructor stub
+	}
+
+
+	@Override
+	public String toString() {
+		return "Users [id=" + id + ", fullname=" + fullname + ", email=" + email + ", password=" + password + "]";
 	}
 	
 	
